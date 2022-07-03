fd7b44c6950d9cf68724e00ee760d07561f7f6ca
Alberto Sara Zaror <>
2022-07-03 00:31:34 -0400

    Introduced models for HomeApp, to be able to dynamically write and add new entries to the site through the django admin.
    This way the texts from the home page and the about section aren't hardcoded.

--- MLDJangoDemo/HomeApp/views.py
+++ MLDJangoDemo/HomeApp/views.py
@@ -1,11 +1,24 @@
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import HomeTextCard
+from .models import AboutTextSection
+
 
 # Class based views are extremely elegant
 
 
-class HomeView(TemplateView):
+class HomeView(ListView):
     template_name = 'home.html'
+    model = HomeTextCard
+    queryset = HomeTextCard.objects.order_by('-date', '-id')
 
 
-class AboutView(TemplateView):
+class HomeCardView(DetailView):
+    template_name = 'card.html'
+    model = HomeTextCard
+
+
+class AboutView(ListView):
     template_name = 'about.html'
+    model = AboutTextSection
